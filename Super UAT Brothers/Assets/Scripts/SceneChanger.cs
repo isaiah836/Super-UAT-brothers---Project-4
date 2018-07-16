2af22700674d662adac5a24f8e0cc62909801769
ISAIAH SANCHEZ <ISASANCH@uat.edu>
2018-07-16 13:34:00 -0700

    unfinished scene changer

---
+++ Super UAT Brothers/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public static SceneChanger instanceSM;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (intanceSM == null)
+        {
+            instanceSM = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(GameObject);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            
+        }
+    }
+}
